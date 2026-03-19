f6d6b6ed0c784cbbbb0cf8c6142168a13606d9d3
nedarctic <nedarctic@gmail.com>
2026-03-19 14:48:24 +0300

    new url for downloads

--- media_app/urls.py
+++ media_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MediaUploadView, MediaListView
+from .views import MediaUploadView, MediaListView, MediaDownloadView
 
 urlpatterns = [
     path('upload/', MediaUploadView.as_view(), name='upload'),
     path('files/', MediaListView.as_view(), name='files'),
+    path('files/<int:pk>/download/', MediaDownloadView.as_view(), name='media-download'),
 ]
