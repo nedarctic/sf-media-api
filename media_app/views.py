f9ed2626440a2c3b515e9b23b00472b5ef5cff43
nedarctic <nedarctic@gmail.com>
2026-03-19 12:59:02 +0300

    created views for api

--- media_app/views.py
+++ media_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import MediaUpload
+from .serializers import MediaUploadSerializer
 
-# Create your views here.
+class MediaUploadView(APIView):
+    def post(self, request):
+        serializer = MediaUploadSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MediaListView(APIView):
+    def get(self, request):
+        media = MediaUpload.objects.all()
+        serializer = MediaUploadSerializer(media, many=True)
+        return Response(serializer.data)
