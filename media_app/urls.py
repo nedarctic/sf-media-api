8ab9bc1b0228bb5154ec20f8e0aaba72fe20ceec
nedarctic <nedarctic@gmail.com>
2026-03-24 01:01:13 +0300

    new logo urls

--- media_app/urls.py
+++ media_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('upload/', MediaUploadView.as_view(), name='upload'),
     path('files/', MediaListView.as_view(), name='files'),
     path('files/<int:pk>/download/', MediaDownloadView.as_view(), name='media-download'),
+    
     path('logos/upload/', LogoUploadView.as_view()),
     path('logos/', LogoListView.as_view()),
     path('logos/<int:pk>/download/', LogoDownloadView.as_view()),
