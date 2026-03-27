a2b584845a46931128630e646a2c0fa6bfa2b6e2
nedarctic <nedarctic@gmail.com>
2026-03-27 12:33:45 +0300

    dynamic debug

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG=True
+DEBUG = config('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
